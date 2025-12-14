30b3c9e0cb49c315fade03ceea180448ee64bedf
Abdullah Alnasri <your.email@example.comalnasri52@gmail.com>
2025-12-14 22:21:03 +0300

    style: Update map tiles to CartoDB Voyager for better detail

--- components/map/TalentMap.tsx
+++ components/map/TalentMap.tsx
@@ -1434,7 +1434,7 @@ export default function TalentMap({ searchTerm, selectedCategory, viewMode = 'se
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
             />
             <MapContent searchTerm={searchTerm} selectedCategory={selectedCategory} viewMode={viewMode} />
         </MapContainer>
